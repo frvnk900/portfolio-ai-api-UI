23ab58b965b41aa0e43b093e88d8b5359781b6bc
frank kwizigira <frvnkkwizigira@gmail.com>
2025-10-06 12:02:16 -0900

    Update InputArea.jsx

--- ui/src/components/ui_compoents/InputArea.jsx
+++ ui/src/components/ui_compoents/InputArea.jsx
@@ -25,7 +25,7 @@ const ChatPost = ({ onMessageSent, isStreaming = false }) => {
     setError('');
 
     try {
-      const response = await fetch('https://portfolio-ai-api-frank.onrender.com/chat/post', {
+      const response = await fetch('https://portfolio-ai-api-nabe.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
