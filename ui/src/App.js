3893125ae60c4f476417660a96d4dc100fec58f7
frank kwizigira <frvnkkwizigira@gmail.com>
2025-10-06 10:44:35 -0900

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Menu from './components/ui_compoents/menu';
- 
 
 import About from './components/Pages/About';
 import Contact from './components/Pages/Contact';
@@ -24,7 +23,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/chat" element={<ChatBot />} />
- 
         </Routes>
       </div>
     </Router>
